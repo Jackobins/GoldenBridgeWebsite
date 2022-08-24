fb2d55954b8dfdb5752816179a8b642e7c10cc9a
Jackobins <jackwstwang@gmail.com>
2022-08-24 16:28:47 -0700

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles.css";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Golden Bridge</h2>
+      <div className="nav--buttons">
+        <button>About</button>
+        <button>Products</button>
+        <button>Contact</button>
+      </div>
+    </nav>
+  );
+}
---
+++ src/components/Picbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+
+export default function Picbar() {
+  return (
+    <div className="picbar">
+      <h1>Golden Bridge</h1>
+    </div>
+  );
+}
