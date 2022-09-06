7bb440f7e206fcdcbd20449631332ecd85ab6dd0
Jackobins <jackwstwang@gmail.com>
2022-09-05 17:58:40 -0700

    added counter

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,32 @@ import React from "react";
 import "../styles.css";
 
 export default function Card(props) {
+  const [count, setCount] = React.useState(0);
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => {
+      return prevCount == 0 ? 0 : prevCount - 1;
+    });
+  }
+
   return (
     <div className="card">
       <h3>{props.item.name}</h3>
+      <div className="counter">
+        <button className="counter--minus" onClick={subtract}>
+          -
+        </button>
+        <div className="counter--count">
+          <h3>{count}</h3>
+        </div>
+        <button className="counter--plus" onClick={add}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,81 +2,101 @@ export default [
   {
     id: 1,
     name: "BL201 3Inch",
+    url: "BL201-3",
   },
   {
     id: 2,
     name: "BL201 4Inch",
+    url: "BL201-4",
   },
   {
     id: 3,
     name: "BL600 3Inch",
+    url: "BL600-3",
   },
   {
     id: 4,
     name: "BL600 4Inch",
+    url: "BL600-4",
   },
   {
     id: 5,
     name: "BL601 3Inch",
+    url: "BL601-3",
   },
   {
     id: 6,
     name: "BL601 4Inch",
+    url: "BL601-4",
   },
   {
     id: 7,
     name: "BL2001 3Inch",
+    url: "BL2001-3",
   },
   {
     id: 8,
     name: "BL2001 4Inch",
+    url: "BL2001-4",
   },
   {
     id: 9,
     name: "BL2002 3Inch",
+    url: "BL2002-3",
   },
   {
     id: 10,
     name: "BL2002 4Inch",
+    url: "BL2002-4",
   },
   {
     id: 11,
     name: "BL2003 3Inch",
+    url: "BL2003-3",
   },
   {
     id: 12,
     name: "BL2003 4Inch",
+    url: "BL2003-4",
   },
   {
     id: 13,
     name: "BL2004 3Inch",
+    url: "BL2004-3",
   },
   {
     id: 14,
     name: "BL2004 4Inch",
+    url: "BL2004-4",
   },
   {
     id: 15,
     name: "BL2005 3Inch",
+    url: "BL2005-3",
   },
   {
     id: 16,
     name: "BL2005 4Inch",
+    url: "BL2005-4",
   },
   {
     id: 17,
     name: "BL900 3Inch",
+    url: "BL900-3",
   },
   {
     id: 18,
     name: "BL900 4Inch",
+    url: "BL900-4",
   },
   {
     id: 19,
     name: "BL901 3Inch",
+    url: "BL901-3",
   },
   {
     id: 20,
     name: "BL901 4Inch",
+    url: "BL901-4",
   },
 ];
